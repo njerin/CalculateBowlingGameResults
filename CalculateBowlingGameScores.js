96e8d616393d6bcb1dfadfe4da354fa0fdcb6773
njerin <njerin1@gmail.com>
2017-04-19 23:34:40 -0400

    2nd push

--- CalculateBowlingGameScores.js
+++ CalculateBowlingGameScores.js
@@ -2,6 +2,7 @@ var app = angular.module('app', []);
     app.controller('ctrl', function($scope) {
         $scope.hi = "hi!!";
         $scope.frame = {};
+        $scope.frameScore;
         $scope.calculateScores = function(frame) {
             console.log($scope.frame);
             console.log($scope.frame.frame1a);
@@ -10,9 +11,17 @@ var app = angular.module('app', []);
                 console.log("Strike!!!");
                 $scope.frame.frame1b = null;
                 console.log($scope.frame.frame1b);
+                $scope.frameScore == 10;
             } else {
                 console.log($scope.frame.frame1a);
                 console.log($scope.frame.frame1b);
+                var frame1aScore = $scope.frame.frame1a;
+                var frame1bScore = $scope.frame.frame1b;
+                $scope.frameScore = +frame1aScore + +frame1bScore;
+                console.log($scope.frameScore);
+                if($scope.frameScore==10){
+                    console.log("Spare!");
+                }
             }
 //            for(var i=0; i<$scope.frame.frame1; i++) {
 //                console.log($scope.frame.value);
